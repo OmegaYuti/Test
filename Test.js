d81c07347913af25818664fd3ffa7ef56aba03d8
OmegaYuti <67691347+OmegaYuti@users.noreply.github.com>
2022-11-16 11:52:21 +0300

    Create Test.js

---
+++ Test.js
@@ -0,0 +1,3 @@
+document.getElementById("link_logo").style.cssText+="background: url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqhoBGaq_LtPaN_I0ZUKiYfE5gNsYWcjS4pLvzB6RvsGWU0Woe6mR7MZFd12hL5ar5-x8&usqp=CAU)";
+var elements = document.body.getElementsByTagName("*");
+elements.forEach((element) => {element.style.background = "#71bb86";})
