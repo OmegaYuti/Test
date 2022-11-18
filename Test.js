908f4a2e07dc91f721e536768c54837757633732
OmegaYuti <67691347+OmegaYuti@users.noreply.github.com>
2022-11-18 15:22:34 +0300

    Update Test.js

--- Test.js
+++ Test.js
@@ -6,7 +6,7 @@ li.innerHTML = "D";
 ul.setAttribute("data-time","2600");
 ul.append(li);
 div.append(ul);
-var canvas = document.CreateElement("canvas");
+var canvas = document.createElement("canvas");
 var a = document.getElementsByClassName("viewtablewhite")[1].firstElementChild.firstElementChild.firstElementChild.lastElementChild.firstElementChild.children[5];
 a.append(div);
 a.append(canvas);
